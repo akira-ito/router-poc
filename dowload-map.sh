dbf481f0f5ef884e45ce7b1d38296f3bd3730e5b
Edson Akira Ito <edson.akira21@gmail.com>
2025-04-09 05:23:01 -0300

    feat: adding docker compose

---
+++ dowload-map.sh
@@ -0,0 +1,6 @@
+
+wget -O ./data/osrm/cayman_islands-latest.osm.pbf https://download.openstreetmap.fr/extracts/central-america/cayman_islands-latest.osm.pbf
+
+docker run --user "1001:1001" -t -v ./data/osrm:/data ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/car.lua /data/cayman_islands-latest.osm.pbf
+docker run --user "1001:1001" -t -v ./data/osrm:/data ghcr.io/project-osrm/osrm-backend osrm-partition /data/cayman_islands-latest.osrm
+docker run --user "1001:1001" -t -v ./data/osrm:/data ghcr.io/project-osrm/osrm-backend osrm-customize /data/cayman_islands-latest.osrm
